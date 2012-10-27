5381a9dea74ec239abead5ca0774bfddcf824617
Andy Karpov <andy.karpov@gmail.com>
2012-10-27 16:44:15 +0300

    Fixed bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,10 +7,11 @@ date_default_timezone_set('Europe/Kiev');
 error_reporting(E_ALL);
 
 require_once 'lib/PhpSerial/PhpSerial.class.php';
-require_once 'lib/MPD/MPD.class.php';
-require_once 'class/Application.class.php';
+require_once 'class/ApplicationAbstract.class.php';
+require_once 'class/ApplicationCli.class.php';
 require_once 'class/LCD.class.php';
 require_once 'class/M3U.class.php';
+require_once 'class/MPD.class.php';
 require_once 'class/Player.class.php';
 require_once 'class/Serial.class.php';
 require_once 'class/Stations.class.php';
